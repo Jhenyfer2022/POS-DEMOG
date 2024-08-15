9290bae7b5deee4ad722453190ff6ed93fb3411a
Jhenyfer2022 <120232577+Jhenyfer2022@users.noreply.github.com>
2024-08-15 16:33:27 -0400

    otra idea

--- pos_screen_pane_position/static/src/js/numpad_modification.js
+++ pos_screen_pane_position/static/src/js/numpad_modification.js
@@ -4,11 +4,10 @@ odoo.define('your_module_name.NumpadModification', function (require) {
     const NumpadWidget = require('point_of_sale.NumpadWidget');
 
     NumpadWidget.include({
-        getNumpadButtons() {
-            const buttons = this._super();  // Llamar a la función original para obtener todos los botones
-            // Filtrar o modificar los botones que quieres ocultar
+        getNumpadButtons: function () {
+            const buttons = this._super.apply(this, arguments);  // Llamar a la función original para obtener todos los botones
             return buttons.filter(button => {
-                // Ocultar el botón de '7' por ejemplo
+                // Ocultar el botón de '7', por ejemplo
                 if (button.label === '7') {
                     return false; // No incluir este botón
                 }
