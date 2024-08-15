cdca580873d7df5fb0998ff8b472a5656bc88b15
Jhenyfer2022 <120232577+Jhenyfer2022@users.noreply.github.com>
2024-08-15 16:37:16 -0400

    otra idea

--- pos_screen_pane_position/static/src/js/numpad_modification.js
+++ pos_screen_pane_position/static/src/js/numpad_modification.js
@@ -1,11 +1,11 @@
-odoo.define('your_module_name.NumpadModification', function (require) {
+odoo.define('your_module_name.NumpadModification', ['require', 'point_of_sale.NumpadWidget'], function (require) {
     'use strict';
 
     const NumpadWidget = require('point_of_sale.NumpadWidget');
 
     NumpadWidget.include({
         renderElement: function () {
-            this._super();
+            this._super.apply(this, arguments);
             this.$('.button-numpad[data-key="7"]').hide(); // Ocultar el botón '7'
             // Puedes agregar más botones a ocultar de la misma manera
         }
