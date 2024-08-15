e107844b325b8294ec7be77a836e9625d63cb997
Jhenyfer2022 <120232577+Jhenyfer2022@users.noreply.github.com>
2024-08-15 15:16:26 -0400

    comment code

--- pos_hide_specific_zone/static/src/js/numpad.js
+++ pos_hide_specific_zone/static/src/js/numpad.js
@@ -4,7 +4,7 @@ import Registries from 'point_of_sale.Registries';
 const PosButtonRestrict = (PosGlobalState) => class PosButtonRestrict extends PosGlobalState{
 
     async _processData(loadedData){
-        await super._processData(...arguments);
+        await super()._processData(...arguments);
         this.numeric_buttons_visible = loadedData['numeric_buttons_visible']
     }
 }
