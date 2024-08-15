a8f1354b8a9c1806e1349c0d3018d02d195f6dc9
Jhenyfer2022 <120232577+Jhenyfer2022@users.noreply.github.com>
2024-08-15 16:11:20 -0400

    a

--- pos_hide_specific_zone/static/src/js/numpad.js
+++ pos_hide_specific_zone/static/src/js/numpad.js
@@ -1,10 +1,12 @@
 import { PosGlobalState } from 'point_of_sale.models';
 import Registries from 'point_of_sale.Registries';
 
+Registries.Component.extend(PosGlobalState, PosButtonRestrict);
+
 const PosButtonRestrict = (PosGlobalState) => class PosButtonRestrict extends PosGlobalState{
 
     async _processData(loadedData){
-        await super._processData(...arguments);
+        await super()._processData(...arguments);
         this.visible_backspece_btn = loadedData['visible_backspece_btn']
     }
 }
