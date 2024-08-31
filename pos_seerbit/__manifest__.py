e5f175869aa0cb79941b05b56314642711a57bbe
Erick Denis <erickmercadooudalova@gmail.com>
2024-08-31 15:38:27 -0400

    test

--- pos_seerbit/__manifest__.py
+++ pos_seerbit/__manifest__.py
@@ -11,14 +11,28 @@
         'data/pos.payment.method.csv',
         'views/res_config_settings_views.xml',
         'views/pos_payment_method_views.xml',
+        ''
     ],
     'depends': ['point_of_sale'],
     'installable': True,
+    #'assets': {
+    #    'point_of_sale.assets': [
+    #        'pos_seerbit/static/src/**/*',
+    #    ],
+    #},
+
     'assets': {
-        'point_of_sale.assets': [
-            'pos_seerbit/static/src/**/*',
+        'point_of_sale._assets_pos': [
+            'pos_seerbit/static/src/js/models.js',
+            'pos_seerbit/static/src/js/payment_seerbit.js',
+            'pos_seerbit/static/src/js/PaymentScreen.js',
+
+            'pos_seerbit/static/src/scss/pos.scss',
+
+            'pos_seerbit/static/src/xml/PaymentScreenPaymentLines.xml',
         ],
     },
+
     'license': 'OPL-1',
     'images': ['static/description/seerbit.gif']
 }
