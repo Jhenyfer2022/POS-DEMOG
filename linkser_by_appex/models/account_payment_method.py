65bbf830f24003443a1ffa8c20531fef3e9c670e
Erick Denis <erickmercadooudalova@gmail.com>
2024-08-30 20:10:07 -0400

    fix error provider

--- linkser_by_appex/models/account_payment_method.py
+++ linkser_by_appex/models/account_payment_method.py
@@ -7,7 +7,13 @@ class AccountPaymentMethod(models.Model):
 
     @api.model
     def _get_payment_method_information(self):
-        """ Payment method for linkser """
+        #""" Payment method for linkser """
+        #res = super()._get_payment_method_information()
+        #res['linkser'] = {'mode': 'multi', 'domain': [('type', '=', 'bank')]}
+        #return res
+
+        """ Integrate Linkser as a provider under the Card payment method """
         res = super()._get_payment_method_information()
-        res['linkser'] = {'mode': 'multi', 'domain': [('type', '=', 'bank')]}
+        if 'card' in res:
+            res['card']['domain'] = [('type', '=', 'bank')]  # o cualquier otra configuración que necesites
         return res
