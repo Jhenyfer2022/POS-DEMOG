78d16b89a16ae1d743a58fbadd14e6839cd29ede
Jhenyfer2022 <leonardo.eguez@appexbo.com>
2024-09-03 14:04:39 -0400

    get product scaned

--- bi_pos_default_customer_and_hide_areas/static/src/overrides/models/models.js
+++ bi_pos_default_customer_and_hide_areas/static/src/overrides/models/models.js
@@ -2,16 +2,14 @@
 
 import { Order, Orderline, Payment } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
 
 patch(Order.prototype, {
 
-    async setup() {
+    setup() {
         super.setup(...arguments);
         var default_customer = this.pos.config.res_partner_id;
         var default_customer_by_id = this.pos.db.get_partner_by_id(default_customer[0]);
-        this.pos = usePos();
-
+        
         if(default_customer_by_id){
             this.set_partner(default_customer_by_id);
         } else{
